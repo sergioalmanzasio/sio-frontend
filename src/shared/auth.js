2235d47121781206eac1ee9170870ec30e525ab6
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-14 20:52:16 -0500

    feat: archivo admisitrador de funcionalidades de manejo local del login

---
+++ src/shared/auth.js
@@ -0,0 +1,36 @@
+/**
+ * Saves user data to localStorage.
+ * @param {object} data - The user data object from the API response.
+ */
+
+export const saveUserData = (data) => {
+  const { role_id, role_name, person, username } = data.data;
+  const userData = {
+    firstName: person.first_name,
+    middleName: person.middle_name,
+    lastName: person.last_name,
+    roleId: role_id,
+    roleName: role_name,
+    username: username,
+  };
+  localStorage.setItem("userData", JSON.stringify(userData));
+};
+
+/**
+ * Retrieves user data from localStorage.
+ * @returns {object|null} The parsed user data object or null if not found.
+ */
+export const getUserData = () => {
+  const userDataJSON = localStorage.getItem('userData');
+  if (userDataJSON) {
+    return JSON.parse(userDataJSON);
+  }
+  return null;
+};
+
+/**
+ * Removes user data and token from localStorage.
+ */
+export const clearUserData = () => {
+  localStorage.clear();
+};
