f23a7af2be17ce9430878119218efbb66b519939
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-23 12:09:19 -0500

    refactor: implementación de filtrado con base a seleccion

--- src/components/header/OfferHeader.jsx
+++ src/components/header/OfferHeader.jsx
@@ -1,12 +1,12 @@
 
 import { useEffect, useState } from "react";
 import Select from "../ui/select";
-import { Search } from "lucide-react";
+import { Search, ListFilter, RefreshCcw } from "lucide-react";
 import ToastAlert from "../alerts/ToastAlert";
 import useOperator from "../../hooks/useOperator";
 import useCategory from "../../hooks/useCategory";
 
-const OfferHeader = ({isLoadingOffers = true}) => {
+const OfferHeader = ({isLoadingOffers = true, onSearch}) => {
   const [service, setService] = useState("");
   const [operator, setOperator] = useState("");
 
@@ -24,8 +24,20 @@ const OfferHeader = ({isLoadingOffers = true}) => {
       });
       return false; // Detiene el envío
     }
-    resetForm();
+    
+    const operatorSelected = operator === "Todos" ? {id: "Todos"} : fetchedOperators.find(op => op.name === operator);
+    const serviceSelected = service === "Todos" ? {id: "Todos"} : fetchedCategories.find(cat => cat.name === service)
+    // Manda los IDs al padre
+    onSearch(
+      operatorSelected === null || operatorSelected === undefined ? null : operatorSelected.id, 
+      serviceSelected === null || serviceSelected === undefined ? null : serviceSelected.id);
+    // resetForm();
     // TODO: Implementar la lógica de búsqueda 
+  };
+
+  const handleReset = () => {
+    resetForm();
+    onSearch(null, null);
   };
 
   // Get Operator to select
@@ -37,12 +49,20 @@ const OfferHeader = ({isLoadingOffers = true}) => {
 
   // Si deseas mantener una lista por defecto hasta que carguen:
   const OPERATORS_LIST = fetchedOperators && fetchedOperators.length > 0
-    ? fetchedOperators.map(op => op.name) // Ajusta esta línea según la estructura de los datos que devuelve tu API.
+    ? fetchedOperators.map(op => op.name) 
     : [];
 
+  // TODO: Verificar si es necesario agregar el operador "Todos"  
+  // Agregar operador llamado "Todos" al final de la lista
+  OPERATORS_LIST.push("Todos");
+
   const SERVICES_LIST = fetchedCategories && fetchedCategories.length > 0
-    ? fetchedCategories.map(cat => cat.name) // Ajusta esta línea según la estructura de los datos que devuelve tu API.
+    ? fetchedCategories.map(cat => cat.name) 
     : [];
+
+  // TODO: Verificar si es necesario agregar el servicio "Todos"  
+  // Agregar servicio llamado "Todos" al final de la lista
+  SERVICES_LIST.push("Todos");
   
   const resetForm = () => {
     setOperator("");
@@ -52,10 +72,10 @@ const OfferHeader = ({isLoadingOffers = true}) => {
   return (
     <div className="flex flex-col md:flex-row gap-2 w-full justify-center p-6 md:p-0">
       {/* Contenedor Intermedio para centrar el grupo de 3/4 */}
-      <div className="flex flex-col md:flex-row gap-2 md:w-3/4 lg:w-1/2">
+      <div className="flex flex-col md:flex-row gap-2 md:w-6/6 lg:w-1/2">
         
         {/* Select operadores (w-1/3 del grupo) */}
-        <div className="w-full md:w-1/3">
+        <div className="w-full md:w-3/6">
           <Select 
             options={OPERATORS_LIST} 
             label="operador" 
@@ -64,7 +84,7 @@ const OfferHeader = ({isLoadingOffers = true}) => {
         </div>
         
         {/* Select servicios (w-1/3 del grupo) */}
-        <div className="w-full md:w-1/3">
+        <div className="w-full md:w-3/6">
           <Select 
             options={SERVICES_LIST} 
             label="servicio" 
@@ -73,12 +93,19 @@ const OfferHeader = ({isLoadingOffers = true}) => {
         </div>
         
         {/* Button search (w-1/3 del grupo) */}
-        <div className="w-full md:w-1/3">
+        <div className="w-full md:w-1/6">
           {/* El botón debe llenar su contenedor de 1/3 en mobile y desktop */}
-          <button className="w-full h-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed" onClick={handleSearch} disabled={ !operator && !service || isLoadingOffers}>
-            Buscar <Search size={20} className="ml-2"/>
+          <button className="w-full h-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer" onClick={handleSearch} disabled={ !operator && !service || isLoadingOffers}>
+            Buscar <ListFilter size={20} className="ml-2"/>
           </button>
         </div>
+
+        <div className="w-full md:w-1/6">
+            {/* El botón debe llenar su contenedor de 1/3 en mobile y desktop */}
+            <button className="w-full h-full bg-lime-300 text-white px-4 py-2 rounded-md hover:bg-lime-400 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer btn-gradient-secondary" onClick={handleReset} disabled={ isLoadingOffers}>
+              <RefreshCcw size={20} className="ml-2"/>
+            </button>
+          </div>  
         
       </div>
     </div>
