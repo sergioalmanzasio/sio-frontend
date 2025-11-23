0511fdfce10424c1ca2d09f2ace81f566c27ddb2
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-23 12:15:19 -0500

    refactor: implementacioón de solicitudes de servicio completa

--- src/sections/bentro-grid/BentoGridSectionOffers.jsx
+++ src/sections/bentro-grid/BentoGridSectionOffers.jsx
@@ -5,16 +5,27 @@ import Card from "../../components/card/Card";
 import ToastAlert from "../../components/alerts/ToastAlert";
 import OfferDetailModal from "../../components/alerts/OfferDetailModal";
 import useBenefit from "../../hooks/useBenefit";
-import { Star } from "lucide-react";
 import { OPERATORS_LOGOS } from "../../shared/utils";
+import { useAuth } from "../../context/AuthContext";
+import OfferBuyBottomModal from "../../components/modals/OfferBuyBottomModal";
+import useRequest from "../../hooks/useRequest";
+import FullScreenLoader from "../../components/ui/FullScreenLoader";
+
+
 
 export default function BentoGridSectionOffers( { offers = [] } ) {
 
-  const { benefits, loading, error, getBenefits } = useBenefit();
+  const { benefits, error, getBenefits } = useBenefit();
+  const { validateClienteRequestPending} = useRequest();
+  const [offerId, setOfferId] = useState("");
   const [offerDescription, setOfferDescription] = useState("");
   const [offerTitle, setOfferTitle] = useState("");
   const [offerPrice, setOfferPrice] = useState("");
   const [offerOperator, setOfferOperator] = useState("");
+  const { isAuthenticated, userData } = useAuth();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [code, setCode] = useState("");
+  const [loadingAddRequest, setLoadingAddRequest] = useState(false);
 
   useEffect(() => {
     if (benefits) {
@@ -67,11 +78,49 @@ export default function BentoGridSectionOffers( { offers = [] } ) {
   };
 
 
+  const handleBuyClick = async (id, title, description, price, operator) => {
+    setOfferId(id);
+    clearInputCode();
+      
+    if (!isAuthenticated) {
+      ToastAlert({
+        position: "center",
+        timer: 2000,
+        icon: "info",
+        title: "Debe iniciar sesión para adquirir la oferta.",
+        isColored: false
+      });
+      return;
+    }
 
-  const handleBuyClick = (id) => {
-    console.log("Comprar", id);
-    // TODO: Validar si el usuario está autenticado
-    // TODO: Implementar la lógica de búsqueda y mostrar modal con la información de la compra
+    // Validar si el usuario ya tiene una solicitud en estado de pendiente/en progreso
+    try {
+      const result = await validateClienteRequestPending({
+        email: userData?.email // Usar el email del usuario autenticado
+      });
+      if (result.hasPendingRequest || result.process === 'session-expired') {
+        
+        return;
+      }
+      // Proceder con la compra
+      setOfferDescription(description);
+      setOfferTitle(title);
+      setOfferPrice(price);
+      setOfferOperator(operator);
+      setIsModalOpen(true);
+    } catch (error) {
+      // console.error("Error validando solicitud pendiente:", error);
+    }
+  };
+
+
+  // Manejando el cambio del código del asesor
+  const handleCodeChange = (e) => {
+    setCode(e.target.value);
+  };
+
+  const clearInputCode = ( ) => {
+    setCode("");
   };
 
   let GRID_ITEMS = [];
@@ -86,38 +135,77 @@ export default function BentoGridSectionOffers( { offers = [] } ) {
     }));
   }
 
-  return (
+  useEffect(() => {
+    if (loadingAddRequest) {
+      document.body.style.overflow = "hidden";   // Bloquea scroll
+    } else {
+      document.body.style.overflow = "";         // Restaura scroll
+    }
+  }, [loadingAddRequest]);
 
-    <section className="py-20 px-6 bg-gray-50">
-      {/* GRID PRINCIPAL:
-              - Móvil: 1 columna (gap-4)
-              - Desktop (lg): 4 columnas (grid-cols-4) con un gap-8
-            */}
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
-        {GRID_ITEMS.map((item, idx) => (
-          <div
-            key={item.id}
-            className={`
-                        col-span-1 
-                        /* Aplicamos el desplazamiento solo a partir de LG (desktop) */
-                        lg:mt-0 
-                        
-                        /* Lógica del Bento: Desplazar las columnas pares (col 2, 4) */
-                        ${ (idx % 4 === 1 || idx % 4 === 3) ? "lg:translate-y-10" : "" }
-                    `}
-          >
-            <Card
-              title={item.title}
-              description={item.description}
-              index={item.index}
-              operator={item.operator}
-              price={item.price}
-              onBenefitsClick={() => handleBenefitsClick(item.id, item.title, item.description, item.price, item.operator)}
-              onBuyClick={() => handleBuyClick(item.id)}
-            />
+  return (
+    <>
+      <FullScreenLoader show={loadingAddRequest} message="Procesando solicitud, espere un momento." />
+      <section className={`${loadingAddRequest ? 'pointer-events-none' : 'py-4 md:py-12 px-6 bg-gray-50'} `}>
+        {/* GRID PRINCIPAL:
+                - Móvil: 1 columna (gap-4)
+                - Desktop (lg): 4 columnas (grid-cols-4) con un gap-8
+              */}
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-8">
+          {GRID_ITEMS.map((item, idx) => (
+            <div
+              key={item.id}
+              className={`
+                          col-span-1 
+                          /* Aplicamos el desplazamiento solo a partir de LG (desktop) */
+                          lg:mt-0 
+                          
+                          /* Lógica del Bento: Desplazar las columnas pares (col 2, 4) */
+                          ${ (idx % 4 === 1 || idx % 4 === 3) ? "lg:translate-y-10" : "" }
+                      `}
+            >
+              <Card
+                title={item.title}
+                description={item.description}
+                index={item.index}
+                operator={item.operator}
+                price={item.price}
+                onBenefitsClick={() => handleBenefitsClick(item.id, item.title, item.description, item.price, item.operator)}
+                onBuyClick={() => handleBuyClick(item.id, item.title, item.description, item.price, item.operator)}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
+    
+      {/* ⬇️ Modal renderizado correctamente */}
+      <OfferBuyBottomModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Detalle de la oferta"
+        code={code}
+        offerID={offerId}
+        email={userData?.email}
+        onLoadingChange={(state) => setLoadingAddRequest(state)}
+      >
+        <div className="flex flex-col gap-2 text-left">
+          <div className="w-full flex flex-col align-left md:flex-row justify-left items-center">
+            <img src={OPERATORS_LOGOS[offerOperator.toUpperCase()] ?? OPERATORS_LOGOS.CLARO} alt={offerOperator} className="w-48 h-14 object-contain"/>
+            <div className="w-full">
+              <h2 className="text-2xl font-semibold">{offerTitle}</h2>
+              <h3 className="text-xl">{offerDescription}</h3>
+              <p className="text-2xl 
+              bg-linear-to-r from-red-500 via-green-500 to-purple-500 text-transparent bg-clip-text
+              font-semibold text-left">{offerPrice}<span className="text-gray-600 text-sm font-normal"> /mes</span></p>
+            </div>
           </div>
-        ))}
-      </div>
-    </section>
+          <div className="h-px w-full bg-gray-200 my-4"></div>
+          <p className="text-md text-gray-600 font-normal text-left">¿Recibió apoyo de un asesor?, por favor digite el código del asesor.</p>
+          <div className="flex flex-row items-center gap-2">
+            <input className="w-full border border-gray-200 rounded-md p-4" type="text" id="code" name="code" value={code} placeholder="Código del asesor" onChange={handleCodeChange}/>
+          </div>
+        </div>
+      </OfferBuyBottomModal>
+    </>
   );
 }
