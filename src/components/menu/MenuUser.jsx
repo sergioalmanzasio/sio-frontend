ca208bedc2e8e9c0df92023c095472e370bc646d
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-23 21:09:47 -0500

    refactor: obtención de menu de usuario basado en role

--- src/components/menu/MenuUser.jsx
+++ src/components/menu/MenuUser.jsx
@@ -1,6 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { X,ChevronUpCircle } from "lucide-react";
+import useMenu from "../../hooks/useMenu";
+import { useAuth } from "../../context/AuthContext";
+
+
 
 const MenuUser = ({ onClose, onCloseSession }) => {
+  const { isAuthenticated, userData } = useAuth();
+  const { getOptionByRole, loading, menus } = useMenu();
+  const roleID = userData?.roleId;
+  const navigate = useNavigate();
+  
+  const getMenu = async () => {
+    if (roleID) {
+      const options = await getOptionByRole(roleID);
+      return options;
+    }
+    return [];
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, [roleID]);
+
+
   return (
     <div className="absolute right-0 mt-4 w-80 md:w-100 bg-white border border-gray-100 rounded-xl shadow-2xl p-6 origin-top-right transform scale-100 transition-all duration-300 ease-out animate-fadeIn">
       <div className="relative">
@@ -12,17 +36,26 @@ const MenuUser = ({ onClose, onCloseSession }) => {
         <ChevronUpCircle className="cursor-pointer"/>
       </button>     
       <ul className="space-y-2">
+        {
+          menus && menus.length > 0 ? (
+            menus?.map((menu) => (
+              <li key={menu.id} className="flex items-center gap-2 cursor-pointer ">
+                <span className="text-gray-600 hover:text-blue-600 transition" onClick={() => navigate(menu.url)}>
+                  {menu.name}</span>
+              </li>
+            ))
+          ) : (
+            <li className="text-gray-500">No hay opciones disponibles</li>
+          )
+        }
+        <div className="h-px w-full bg-gray-200 my-2"></div>
         <li className="flex items-center gap-2 cursor-pointer ">
-          <span className="text-gray-600 hover:text-blue-600 transition">Ofertas</span>
-        </li>
-        <li className="flex items-center gap-2 cursor-pointer ">
-          <span className="text-gray-600 hover:text-blue-600 transition">Configuración</span>
+          <span className="text-gray-600 hover:text-red-600 transition" onClick={() => navigate('/config')}>Configuración</span>
         </li>
         <li className="flex items-center gap-2 cursor-pointer ">
           <span className="text-gray-600 hover:text-red-600 transition" onClick={onCloseSession}>Cerrar sesión</span>
         </li>
       </ul>
-      
     </div>
   );
 };
