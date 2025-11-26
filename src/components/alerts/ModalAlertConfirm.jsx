a07a883d9827ff02933fc846d44183cf866eb692
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-26 16:51:00 -0500

    refactor: actualización de implementación

--- src/components/alerts/ModalAlertConfirm.jsx
+++ src/components/alerts/ModalAlertConfirm.jsx
@@ -1,26 +1,22 @@
 import Swal from "sweetalert2";
 
 export default function ModalAlertConfirm({ title, text, icon, confirmText, cancelText, confirmCallback, cancelCallback, isShowConfirmButton = true, isShowCancelButton = true, isAllowOutsideClick = false }) {
-  return new Promise((_, reject) => {
-    Swal.fire({
-      title: title,
-      text: text,
-      icon: icon,
-      showConfirmButton: isShowConfirmButton,
-      showCancelButton: isShowCancelButton,
-      confirmButtonText: confirmText,
-      cancelButtonText: cancelText,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      allowOutsideClick: isAllowOutsideClick,
-      
-    }).then((result) => {
-      if (result.isConfirmed) {
-        confirmCallback();
-      } else {
-        // reject();
-      }
-    });
+  return Swal.fire({
+    title: title,
+    text: text,
+    icon: icon,
+    showConfirmButton: isShowConfirmButton,
+    showCancelButton: isShowCancelButton,
+    confirmButtonText: confirmText,
+    cancelButtonText: cancelText,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    allowOutsideClick: isAllowOutsideClick,
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      if (confirmCallback) await confirmCallback();
+    } else {
+      if (cancelCallback) await cancelCallback();
+    }
   });
 }
-
