9c866dc49ed5df83bc42a08582e463fc7998a007
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-26 16:51:18 -0500

    refactor: se agrega titulo

--- src/components/header/OfferHeader.jsx
+++ src/components/header/OfferHeader.jsx
@@ -70,45 +70,49 @@ const OfferHeader = ({isLoadingOffers = true, onSearch}) => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row gap-2 w-full justify-center p-6 md:p-0">
-      {/* Contenedor Intermedio para centrar el grupo de 3/4 */}
-      <div className="flex flex-col md:flex-row gap-2 md:w-6/6 lg:w-1/2">
-        
-        {/* Select operadores (w-1/3 del grupo) */}
-        <div className="w-full md:w-3/6">
-          <Select 
-            options={OPERATORS_LIST} 
-            label="operador" 
-            value={operator} 
-            onChange={(e) => setOperator(e.target.value)} />
-        </div>
-        
-        {/* Select servicios (w-1/3 del grupo) */}
-        <div className="w-full md:w-3/6">
-          <Select 
-            options={SERVICES_LIST} 
-            label="servicio" 
-            value={service} 
-            onChange={(e) => setService(e.target.value)} />
-        </div>
-        
-        {/* Button search (w-1/3 del grupo) */}
-        <div className="w-full md:w-1/6">
-          {/* El botón debe llenar su contenedor de 1/3 en mobile y desktop */}
-          <button className="w-full h-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer" onClick={handleSearch} disabled={ !operator && !service || isLoadingOffers}>
-            Buscar <ListFilter size={20} className="ml-2"/>
-          </button>
-        </div>
-
-        <div className="w-full md:w-1/6">
-            {/* El botón debe llenar su contenedor de 1/3 en mobile y desktop */}
-            <button className="w-full h-full bg-lime-300 text-white px-4 py-2 rounded-md hover:bg-lime-400 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer btn-gradient-secondary" onClick={handleReset} disabled={ isLoadingOffers}>
-              <RefreshCcw size={20} className="ml-2"/>
-            </button>
-          </div>  
-        
+    <>
+      <div className="w-full md:w-6/6">
+        <h3 className="text-lg font-semibold mb-4 text-center">Filtros de búsqueda</h3>
       </div>
-    </div>
+      <div className="flex flex-col md:flex-row gap-2 w-full justify-center p-0 md:p-0">
+        {/* Contenedor Intermedio para centrar el grupo de 3/4 */}
+        <div className="flex flex-col md:flex-row gap-2 md:w-6/6 lg:w-1/2">
+          {/* Select operadores (w-1/3 del grupo) */}
+          <div className="w-full md:w-3/6">
+            <Select 
+              options={OPERATORS_LIST} 
+              label="operador" 
+              value={operator} 
+              onChange={(e) => setOperator(e.target.value)} />
+          </div>
+          
+          {/* Select servicios (w-1/3 del grupo) */}
+          <div className="w-full md:w-3/6">
+            <Select 
+              options={SERVICES_LIST} 
+              label="servicio" 
+              value={service} 
+              onChange={(e) => setService(e.target.value)} />
+          </div>
+          
+          {/* Button search (w-1/3 del grupo) */}
+          <div className="w-full md:w-1/6">
+            {/* El botón debe llenar su contenedor de 1/3 en mobile y desktop */}
+            <button className="w-full h-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer" onClick={handleSearch} disabled={ !operator && !service || isLoadingOffers}>
+              Buscar <ListFilter size={20} className="ml-2"/>
+            </button>
+          </div>
+
+          <div className="w-full md:w-1/6">
+              {/* El botón debe llenar su contenedor de 1/3 en mobile y desktop */}
+              <button className="w-full h-full bg-lime-300 text-white px-4 py-2 rounded-md hover:bg-lime-400 transition-colors flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer btn-gradient-secondary" onClick={handleReset} disabled={ isLoadingOffers}>
+                <RefreshCcw size={20} className="ml-2"/>
+              </button>
+            </div>  
+          
+        </div>
+      </div>
+    </>
   );
 };
 
