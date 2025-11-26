616fc63bb863c232d3d8cfa20c236a7516ab5730
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-26 16:52:01 -0500

    refactor: nuevos endpoint

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -10,7 +10,9 @@ const useRequest = () => {
   const [loading, setLoading] = useState(false);
   const [loadingAddRequest, setLoadingAddRequest] = useState(false);
   const [loadingServiceRequestClient, setLoadingServiceRequestClient] = useState(false);
+  const [loadingServiceRequestsByServiceCoordinator, setLoadingServiceRequestsByServiceCoordinator] = useState(false);
   const [loadingServiceRequestDetail, setLoadingServiceRequestDetail] = useState(false);
+  const [loadingCancelServiceRequestClient, setLoadingCancelServiceRequestClient] = useState(false);
   const [error, setError] = useState(null);
 
   const addServiceRequest = useCallback(async (requestData, options = {}) => {
@@ -189,7 +191,91 @@ const useRequest = () => {
     }
   }, []);
 
-  return { loading, loadingAddRequest, loadingServiceRequestClient, loadingServiceRequestDetail, error, addServiceRequest, validateClienteRequestPending, getServiceRequestByClient, getServiceRequestDetailByID };
+  const cancelServiceRequestByClient = useCallback(async (id, email) => {
+    setLoadingCancelServiceRequestClient(true);
+    setError(null);
+    try {
+      const response = await fetch(`${API_BASE_URL}/request/cancel-by-client`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+          service_request_id: id,
+          email
+        })
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        if (response.status === 401) {
+          sessionExpiredToast(
+            logout, 
+            () => {
+              window.location.href = '/';
+            }
+          );
+          return { process: 'session-expired' };
+        }
+        throw new Error(data.message || "Error en la solicitud");
+      }
+      return data;
+    } catch (err) {
+      setError(err.message);
+      ToastAlert({
+        position: "top",
+        timer: 1800,
+        icon: "error",
+        title: err.message || "Error de red, inténtelo más tarde",
+      });
+      throw err;
+    } finally {
+      setLoadingCancelServiceRequestClient(false);
+    }
+  }, []);
+
+  const getServiceRequestsByServiceCoordinator = useCallback(async (email, options = {}) => {
+    setLoadingServiceRequestsByServiceCoordinator(true);
+    setError(null);
+    try {
+      const response = await fetch(`${API_BASE_URL}/request/service-coordinator`, {
+        ...options,
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email })
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        if (response.status === 401) {
+          sessionExpiredToast(
+            logout, 
+            () => {
+              window.location.href = '/';
+            }
+          );
+          return { process: 'session-expired' };
+        }
+        throw new Error(data.message || "Error en la solicitud");
+      }
+      return data;
+    } catch (err) {
+      setError(err.message);
+      ToastAlert({
+        position: "top",
+        timer: 1800,
+        icon: "error",
+        title: err.message || "Error de red, inténtelo más tarde",
+      });
+      throw err;
+    } finally {
+      setLoadingServiceRequestsByServiceCoordinator(false);
+    }
+  }, []);
+
+  return { loading, loadingAddRequest, loadingServiceRequestClient, loadingServiceRequestsByServiceCoordinator, loadingServiceRequestDetail, loadingCancelServiceRequestClient, error, addServiceRequest, validateClienteRequestPending, getServiceRequestByClient, getServiceRequestDetailByID, cancelServiceRequestByClient, getServiceRequestsByServiceCoordinator };
 };
 
 export default useRequest;
