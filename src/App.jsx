02b48f34f95ff6dca5dd7e53398248c6e6e6dd12
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-27 16:59:18 -0500

    refactor: implementación nuevas rutas

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import LandingPage from "./pages/LandingPage";
 import OffersPage from "./pages/OffersPage";
 import ClientServiceRequests from "./pages/ClientServiceRequests";
 import SysAdminDashboard from "./pages/SysAdminDashboard";
+import CoordinatorServiceRequests from "./pages/CoordinatorServiceRequests";
+import ServiceCoordinatorDashboard from "./pages/ServiceCoordinatorDashboard";
+import AuthRedirect from "./pages/AuthRedirect";
+import SignupPage from "./pages/SignupPage";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
@@ -16,6 +20,10 @@ function App() {
           <Route path="/offers" element={<OffersPage />} />
           <Route path="/client-service-requests" element={<ClientServiceRequests />} />
           <Route path="/sysadmin/dashboard" element={<SysAdminDashboard />} />
+          <Route path="/coordinator-service-requests" element={<CoordinatorServiceRequests />} />
+          <Route path="/coordinator/dashboard" element={<ServiceCoordinatorDashboard />} />
+          <Route path="/auth-redirect" element={<AuthRedirect />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
