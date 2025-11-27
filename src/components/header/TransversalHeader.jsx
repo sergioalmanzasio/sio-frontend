b395279adff0d442ad40fbe2edc66ae0005e491a
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-11-27 17:03:53 -0500

    refactor: nuevo parametro para implementación

--- src/components/header/TransversalHeader.jsx
+++ src/components/header/TransversalHeader.jsx
@@ -1,10 +1,11 @@
 const TransversalHeader = ({
   title = "Transversal Header",
   description = "Esta es una cabecera transversal que se muestra en todas las páginas",
+  classNameAdd = ""
 }) => {
   return (
     <>
-      <div className="flex flex-col w-full md:w-3/4 mt-4 md:mt-8 gap-2 mx-auto justify-start py-2 px-6 md:p-0 bg-white  ">
+      <div className={`flex flex-col w-full md:w-3/4 mt-4 md:mt-8 gap-2 mx-auto justify-start py-2 px-6 md:p-0 ${classNameAdd}`}>
         <h1 className="text-2xl font-bold text-gray-800 w-full">{title}</h1>
         <p className="text-gray-600 w-full">{description}</p>
         <div className="w-full h-1 bg-gray-100 mt-2"></div>
