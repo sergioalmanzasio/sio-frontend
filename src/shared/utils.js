3488e09414164cbb165a974f24ff8418f5c78ea6
Ricardo Velasquez A <rvelasquez@iuvity.com>
2025-12-18 23:14:20 -0500

    refactor: nuevas funcionalidades transversales

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -3,6 +3,7 @@ import movistarLogo from "../assets/logos/MOVISTARLOGO.png";
 import womLogo from "../assets/logos/WOMLOGO.png";
 import tigoLogo from "../assets/logos/TIGOLOGO.png";
 import ToastAlert from "../components/alerts/ToastAlert";
+import { API_COLOMBIA } from "./constanst";
 
 
 export const getColorOpertator = (nameOperator) => {
@@ -69,7 +70,7 @@ export const sessionExpiredToast = (logout, redirectToHome) => {
     icon: "warning",
     title: "Su sesión ha expirado. Por favor, inicie sesión de nuevo.",
   });
-  
+
   setTimeout(() => {
     logout();
     redirectToHome();
@@ -88,3 +89,41 @@ export const chipServiceRequestStatus = (status) => {
   };
   return statusMap[status.toLowerCase()] || status;
 };
+
+export const getRolesToSignUp = () => {
+  return [
+    { id: 'referral', label: 'Plan referido', description: 'Recomienda servicios y gana comisiones.' },
+    { id: 'client', label: 'Soy cliente', description: 'Solicita y gestiona tus servicios.' },
+    { id: 'advisor', label: 'Soy asesor/a', description: 'Brinda asesoría y gestiona solicitudes.' },
+  ];
+};
+
+export const getDocumentTypes = () => {
+  return [
+    { acronym: 'CC', label: 'Cédula de ciudadanía' },
+    { acronym: 'CE', label: 'Cédula de extranjería' },
+    { acronym: 'PA', label: 'Pasaporte' },
+  ];
+};
+
+export const getDepartments = async () => {
+  const urlAPI = `${API_COLOMBIA}/v1/Department`;
+  const response = await fetch(urlAPI);
+  const data = await response.json();
+  const departmentList = data.map((department) => ({
+    id: department.id,
+    name: department.name,
+  }));
+  return departmentList;
+}
+
+export const getCitiesByDepartmentId = async (departmentId) => {
+  const urlAPI = `${API_COLOMBIA}/v1/Department/${departmentId}/cities`;
+  const response = await fetch(urlAPI);
+  const data = await response.json();
+  const cityList = data.map((city) => ({
+    id: city.id,
+    name: city.name,
+  }));
+  return cityList;
+}
