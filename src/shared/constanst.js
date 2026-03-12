cab8ebc8a6584a3987e2a7ecdf16a53aeb8e9bc1
Ricardo Velasquez <rianveal@gmail.com>
2026-03-11 22:16:59 -0500

    feat: constante para manejo de tiempo de inactividad en la plataforma.

--- src/shared/constanst.js
+++ src/shared/constanst.js
@@ -1,3 +1,4 @@
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const API_COLOMBIA = import.meta.env.VITE_API_COLOMBIA;
+export const INACTIVITY_LOGOUT_TIMER = import.meta.env.VITE_INACTIVITY_LOGOUT_TIMER;
