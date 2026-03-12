ae2c45ca9fb89683993716893d997f32a5fe6d7a
Ricardo Velasquez <rianveal@gmail.com>
2026-03-11 23:24:36 -0500

    feat: nuevoas constantes para manejo de contacto con el personal de la empresa de SIO.

--- src/shared/constanst.js
+++ src/shared/constanst.js
@@ -2,3 +2,9 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const API_COLOMBIA = import.meta.env.VITE_API_COLOMBIA;
 export const INACTIVITY_LOGOUT_TIMER = import.meta.env.VITE_INACTIVITY_LOGOUT_TIMER;
+export const CONTACT_EMAIL = import.meta.env.VITE_CONTACT_EMAIL;
+export const CONTACT_PHONE = import.meta.env.VITE_CONTACT_PHONE;
+export const OPENING_HOURS = import.meta.env.VITE_OPENING_HOURS;
+export const FACEBOOK_URL = import.meta.env.VITE_FACEBOOK_URL;
+export const INSTAGRAM_URL = import.meta.env.VITE_INSTAGRAM_URL;
+
